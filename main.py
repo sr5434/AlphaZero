4ca3ab94380f2471bb94e7203d6966778e65a055
Samir <118690585+sr5434@users.noreply.github.com>
2024-02-29 17:04:57 +0000

    Re-add lr

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ import jax.numpy as jnp
 from tqdm import tqdm
 
 class Config(BaseModel):
-    env_id: pgx.EnvId = "kuhn_poker"
+    env_id: pgx.EnvId = input("envId:")
     seed: int = 0
     max_num_iters: int = 50000
     # network params
@@ -31,11 +31,12 @@ class Config(BaseModel):
     num_layers: int = 6
     resnet_v2: bool = True
     # selfplay params
-    selfplay_batch_size: int = 4096
-    num_simulations: int = 32
-    max_num_steps: int = 256
+    selfplay_batch_size: int = 8
+    num_simulations: int = 2
+    max_num_steps: int = 4
     # training params
     training_batch_size: int = 4096
+    learning_rate: float = 0.001
     # eval params
     eval_interval: int = 5
 
