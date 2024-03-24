2b742984e09f712cdcb411a81db083404ca80215
Samir <118690585+sr5434@users.noreply.github.com>
2024-03-24 13:28:50 +0000

    Declutter, add video of gameplay, improve testing script

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # AlphaZero
-Implementing the AlphaZero algorithm(albeit with the MuZero Gumbel policy) for multiple games with PGX and MCTX, as well as MuZero for stock trading with Muax. Thanks to Google's TensorFlow Research Cloud for providing compute resources. The training script is modified from the sample AlphaZero script provided in the PGX GitHub Repo.
+Implementing the AlphaZero algorithm(but with the MuZero Gumbel policy) for multiple games with PGX and MCTX. Thanks to Google's TensorFlow Research Cloud for providing compute resources. The training script is modified from the sample AlphaZero script provided in the PGX GitHub Repo.
 Scripts:
- - ```main.py```: AlphaZero for games(Atari, Go, Poker, etc.)
- - ```muzero_trading.py```: MuZero for BTC trading(with Muax)
- - ```test.py```: Test the trained Bitcoin Trader Bot
- - ```test_model.py```: Test the games models(i.e. Kuhn Poker)
+ - ```main.py```: AlphaZero for playing games(Atari, Go, Poker, etc.)
+ - ```test.py```: Test the model and output an SVG of it playing itself.
 
 
 Checkpoints(more coming soon):
  - [Kuhn Poker](https://huggingface.co/sr5434/AlphaZero-Kuhn-Poker)
- - [Othello(Work in progress)](http://huggingface.co/sr5434/alphazero-othello)
+ - [Othello/Reversi(Work in progress)](http://huggingface.co/sr5434/alphazero-othello)
+
+Othello model plays itself(step 15145):
+![AI plays itself in game of Othello](./game.svg)
+<img src="./game.svg">
--- test.py
+++ test.py
@@ -1,28 +1,134 @@
-"""Test MuZero"""
-import muax
-from muax import nn 
-import gymnasium as gym
+import mctx
+import haiku as hk
+import jax.numpy as jnp
 import jax
+import pgx
+from pydantic import BaseModel
+import pickle
+from pgx.experimental import auto_reset
+from typing import NamedTuple
+
+class Config(BaseModel):
+    env_id: pgx.EnvId = ""
+    seed: int = 0
+    max_num_iters: int = 1
+    # network params
+    num_channels: int = 128
+    num_layers: int = 6
+    resnet_v2: bool = True
+    # selfplay params
+    #Testing
+    selfplay_batch_size: int = 1028
+    num_simulations: int = 32
+    max_num_steps: int = 256
+    # training params
+    training_batch_size: int = 4096
+    learning_rate: float = 0.001
+    # eval params
+    eval_interval: int = 5
+
+    class Config:
+        extra = "forbid"
+
+with open(input("Path to checkpoint: "), "rb") as f:
+    d = pickle.load(f)
+    print(d.keys())
+    model = d["model"]
+    config = d["config"]
+
+env = pgx.make(config.env_id)
+
+class BlockV2(hk.Module):
+    def __init__(self, num_channels, name="BlockV2"):
+        super(BlockV2, self).__init__(name=name)
+        self.num_channels = num_channels
+
+    def __call__(self, x, is_training, test_local_stats):
+        i = x
+        x = hk.BatchNorm(True, True, 0.9)(x, is_training, test_local_stats)
+        x = jax.nn.relu(x)
+        x = hk.Conv2D(self.num_channels, kernel_shape=3)(x)
+        x = hk.BatchNorm(True, True, 0.9)(x, is_training, test_local_stats)
+        x = jax.nn.relu(x)
+        x = hk.Conv2D(self.num_channels, kernel_shape=3)(x)
+        return x + i
 
 
-support_size = 10 
-embedding_size = 8
-discount = 0.99
-num_actions = 2
-full_support_size = int(support_size * 2 + 1)
+class AZNet(hk.Module):
+    """AlphaZero NN architecture."""
 
-repr_fn = nn._init_representation_func(nn.Representation, embedding_size)
-pred_fn = nn._init_prediction_func(nn.Prediction, num_actions, full_support_size)
-dy_fn = nn._init_dynamic_func(nn.Dynamic, embedding_size, num_actions, full_support_size)
+    def __init__(
+        self,
+        num_actions,
+        num_channels: int = 64,
+        num_blocks: int = 5,
+        name="az_net",
+    ):
+        super().__init__(name=name)
+        self.num_actions = num_actions
+        self.num_channels = num_channels
+        self.num_blocks = num_blocks
+        self.resnet_cls = BlockV2
 
-from muax.test import test
-gradient_transform = muax.model.optimizer(init_value=0.02, peak_value=0.02, end_value=0.002, warmup_steps=5000, transition_steps=5000)
-model = muax.MuZero(repr_fn, pred_fn, dy_fn, policy='muzero', discount=discount,
-                    optimizer=gradient_transform, support_size=support_size)
+    def __call__(self, x, is_training, test_local_stats):
+        if config.env_id == "kuhn_poker" or config.env_id == "leduc_holdem":
+            x = x.reshape((x.shape[0], x.shape[1], 1))
+        x = x.astype(jnp.float32)
+        x = hk.Conv2D(self.num_channels, kernel_shape=2)(x)
 
-model.load("./cartpole_model_params.npy")
+        for i in range(self.num_blocks):
+            x = self.resnet_cls(self.num_channels, name=f"block_{i}")(
+                x, is_training, test_local_stats
+            )
+        x = hk.BatchNorm(True, True, 0.9)(x, is_training, test_local_stats)
+        x = jax.nn.relu(x)
 
-env_id = 'CartPole-v1'
-test_env = gym.make(env_id, render_mode='rgb_array')
-test_key = jax.random.PRNGKey(0)
-print(test(model, test_env, test_key, num_simulations=50, num_test_episodes=100, random_seed=None))
+        # policy head
+        logits = hk.Conv2D(output_channels=2, kernel_shape=1)(x)
+        logits = hk.BatchNorm(True, True, 0.9)(logits, is_training, test_local_stats)
+        logits = jax.nn.relu(logits)
+        logits = hk.Flatten()(logits)
+        logits = hk.Linear(self.num_actions)(logits)
+
+        # value head
+        v = hk.Conv2D(output_channels=1, kernel_shape=1)(x)
+        v = hk.BatchNorm(True, True, 0.9)(v, is_training, test_local_stats)
+        v = jax.nn.relu(v)
+        v = hk.Flatten()(v)
+        v = hk.Linear(self.num_channels)(v)
+        v = jax.nn.relu(v)
+        v = hk.Linear(1)(v)
+        v = jnp.tanh(v)
+        v = v.reshape((-1,))
+
+        return logits, v
+
+def forward_fn(x, is_eval=False):
+    net = AZNet(
+        num_actions=env.num_actions,
+        num_channels=config.num_channels,
+        num_blocks=config.num_layers,
+    )
+    policy_out, value_out = net(x, is_training=not is_eval, test_local_stats=False)
+    return policy_out, value_out
+
+forward = hk.without_apply_rng(hk.transform_with_state(forward_fn))
+
+model_params, model_state = model
+init_fn = jax.jit(jax.vmap(env.init))
+step_fn = jax.jit(jax.vmap(env.step))
+
+states = []
+batch_size = 1
+keys = jax.random.split(jax.random.PRNGKey(0), batch_size)
+state = init_fn(keys)
+states.append(state)
+
+while not (state.terminated | state.truncated).all():
+    (logits, value), _ = forward.apply(
+            model_params, model_state, state.observation, is_eval=True
+        )
+    action = logits.argmax(axis=-1)
+    state = step_fn(state, action)
+    states.append(state)
+pgx.save_svg_animation(states, "game.svg", frame_duration_seconds=0.5)
